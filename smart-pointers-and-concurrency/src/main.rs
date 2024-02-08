4e90671751904531aa4d6c059c4767116225cb12
LoganMartinez <lmartin9@trinity.edu>
2024-02-07 23:49:22 -0600

    switched to pi estimation

--- smart-pointers-and-concurrency/src/main.rs
+++ smart-pointers-and-concurrency/src/main.rs
@@ -2,121 +2,75 @@ use std::thread;
 use std::sync::mpsc;
 use std::time::{Instant};
 use rand::Rng;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::{self, BufRead};
 
 
 fn main() {
     let n = 100000000;
-    let generate = false;
     let multi = true;
 
-
-    // single_thread_solve(&n);
-
-    if generate {
-      let res = generate_data(n);
-      match res {
-        Ok(()) => println!("file successfully generated"),
-        Err(e) => println!("{}", e),
-      }
-    }
-
-    // one thread -- 44 seconds
+    // one thread -- 99 seconds
     if !multi {
       let now = Instant::now();
-      let res = single_thread_solve_with_file(&0, &n);
-      match res {
-        Ok(sum) => {
-          println!("sum: {}", sum);
-        },
-        Err(e) => println!("Error: {}", e),
-      }
-      println!("{} sqrts calculated in {} seconds", n, now.elapsed().as_secs());
+      let inside = num_inside(&n);
+      println!("{}", 4.0*(inside as f64)/(n as f64));
+      println!("pi estimated with {} samples in {} seconds", n, now.elapsed().as_secs());
     }
 
-
-    // multiple threads -- 47 seconds
+    // multiple threads -- 20 seconds
     if multi {
       let num_threads = 8;
-      let (tx, rx) = mpsc::channel::<f64>();
+      let (tx, rx) = mpsc::channel::<i32>();
       let now = Instant::now();
-      let lines_per_thread = n/num_threads;
+      let samples_per_thread = n/num_threads;
       for t in 0..num_threads {
         let cloned_tx = mpsc::Sender::clone(&tx);
         thread::spawn(move || {
-          let res = single_thread_solve_with_file(&(lines_per_thread*t), &(lines_per_thread*(t+1)));
-          match res {
-            Ok(sum) => {
-              println!("Thread {} done", t);
-              let _ = cloned_tx.send(sum);
-            },
-            Err(e) => println!("Error on thread {}: {}", t, e),
-          }
+          let inside = num_inside(&samples_per_thread);
+          println!("Thread {} done", t);
+          let _ = cloned_tx.send(inside);
         });
       }
 
-      let mut sum = 0.0;
+      let mut total_inside = 0;
       let mut completed_threads = 0;
       for received in rx {
-        sum += received;
-        println!("new sum: {}", sum);
+        total_inside += received;
         completed_threads += 1;
         if completed_threads == num_threads {
           break;
         }
       }
-      println!("{} sqrts calculated in {} seconds", n, now.elapsed().as_secs());
+      println!("{}",4.0*(total_inside as f64)/(n as f64));
+      println!("pi estimated with {} samples in {} seconds", n, now.elapsed().as_secs());
     }
 }
 
+fn num_inside(samples: &i32) -> i32 {
+  let mut rng = rand::thread_rng();
+  let mut inside = 0;
+  for _ in 0..*samples {
+    let x: f64 = rng.gen();
+    let y: f64 = rng.gen();
+    if x*x+y*y <= 1.0 {
+      inside += 1;
+    }
+  }
+  inside
+}
 
 fn single_thread_solve(n: &i32) {
-    let mut rng = rand::thread_rng();
-    let now = Instant::now();
-    let v: Vec<f64> = (0..*n).map(|_| rng.gen_range(9000.0..100000.0)).collect();
-    let vec_create_time = now.elapsed().as_secs();
-    let mut sum = 0.0;
-    // create time 53 seconds with n=100m
-    println!("created random vec in {} seconds", vec_create_time);
-    
-    for i in v {
-      sum = sum + i.sqrt();
-    }
-    let solve_time = now.elapsed().as_secs()-vec_create_time;
-    // solve time 0 second n=100m
-    println!("{} sqrts calculated in {} seconds", n, solve_time);
-}
-
-fn single_thread_solve_with_file(start_line: &i32, end_line: &i32) -> std::io::Result<f64> {
-  let file = File::open("/Users/logan/Desktop/school/NextGen/smart-pointers-and-concurrency/smart-pointers-and-concurrency/data/data.txt")?;
-  let buffer = io::BufReader::new(file);
-  let mut start_reading = false;
-  let mut sum = 0.0;
-  for (index, line) in buffer.lines().enumerate() {
-    if index == *end_line as usize {
-      break;
-    }
-    if index == *start_line as usize {
-      start_reading = true;
-    }
-    if start_reading {
-      let f: f64 = line?.parse().unwrap();
-      let sqrt = f.sqrt();
-      sum += sqrt;
-    }
-  }
-  Ok(sum)
-}
-
-
-fn generate_data(n: i32) -> std::io::Result<()> {
   let mut rng = rand::thread_rng();
-  let v: Vec<f64> = (0..n).map(|_| rng.gen_range(9000.0..100000.0)).collect();
-  let mut file = File::create("/Users/logan/Desktop/school/NextGen/smart-pointers-and-concurrency/smart-pointers-and-concurrency/data/data.txt")?;
+  let now = Instant::now();
+  let v: Vec<f64> = (0..*n).map(|_| rng.gen_range(9000.0..100000.0)).collect();
+  let vec_create_time = now.elapsed().as_secs();
+  let mut sum = 0.0;
+  // create time 53 seconds with n=100m
+  println!("created random vec in {} seconds", vec_create_time);
+  
   for i in v {
-    writeln!(&mut file, "{}", i)?;
+    sum = sum + i.sqrt();
   }
-  Ok(())
+  let solve_time = now.elapsed().as_secs()-vec_create_time;
+  // solve time 0 second n=100m
+  println!("{} sqrts calculated in {} seconds", n, solve_time);
 }
